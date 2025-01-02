a33d9ae664728a9b88ae6d855020fefa31baaac2
salmakhatun1234umme <ummesalma00569@gmail.com>
2025-01-02 13:40:58 +0600

    added third folder

---
+++ third.js
@@ -0,0 +1,51 @@
+// question no 1
+
+let marks = 50;
+
+if (marks >= 90) {
+    console.log(`${marks} You get A grade`)
+}
+else if (marks >= 80) {
+    console.log(`${marks} You Get B grade`)
+}
+else if (marks >= 70) {
+    console.log(`${marks} You get C grade`)
+}
+else if (marks >= 60) {
+    console.log(`${marks} You get D grade`)
+}
+else {
+    console.log(`${marks} You are fail`)
+}
+
+
+
+
+// question no 2
+let number = 50;
+
+if (number > 0) {
+    console.log(`${number} You are positive number`)
+}
+else if (number < 0) {
+    console.log(`${number} You are negative number`)
+}
+else {
+    console.log(`${number} You are 0`)
+}
+
+
+
+// question no 3 
+let time = 12;
+
+if (time < 12) {
+    console.log(`${time}good morning`);
+}
+
+else if (time >= 12 && time < 17) {
+    console.log(`${time} good evening`)
+}
+else {
+    console.log(`${time} good night`)
+}
